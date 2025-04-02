86ea45ff41a49bd91d2a8fdb7e29bd361af1fd81
tientv <tienco201@gmail.com>
2025-04-02 15:21:16 +0700

    add migration Products

---
+++ prisma/migrations/20250402022958_add_product_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DECIMAL(10,2) NOT NULL,
+    "imageUrl" TEXT,
+    "quantity" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "category" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
